38dcb5c98828dd4cc90b7393d640fdea8b846c8c
Bruno Gomes <bruno.laurentino.adm@gmail.com>
2025-07-04 19:08:54 -0300

    Commit Login

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+HTML
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,11 +15,17 @@
   <link href="template/css/custom-login.css" rel="stylesheet">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+
+  <style>
+    /* Adicionado para definir a cor de fundo purple */
+    body {
+      background-color: purple; /* Você pode usar um código hexadecimal como #800080 para um tom específico de purple */
+      background-image: none; /* Garante que nenhum gradiente padrão seja aplicado */
+    }
+  </style>
 </head>
 
-<body class="bg-gradient-primary">
-
-  <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
+<body> <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
     <div class="col-12">
       <div class="card o-hidden border-0 shadow-lg animate__animated animate__fadeInDown login-card-altura">
         <div class="card-body p-0">
@@ -36,7 +44,7 @@
                   <div class="form-group">
                     <input type="password" class="form-control form-control-user" name="senha" placeholder="Digite sua senha" required>
                   </div>
-                  <button class="btn btn-primary btn-user btn-block" name="login" type="submit">
+                  <button class="btn btn-primary btn-user btn-block btn-purple" name="login" type="submit">
                     <i class="fas fa-sign-in-alt"></i> Acessar
                   </button>
                 </form>
